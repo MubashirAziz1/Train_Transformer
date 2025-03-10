8d0d3956131b8927ab8e7b77ac68f654d14c1ab0
Mubashir <cs.mubashir.a@gmail.com>
2025-03-10 05:52:16 +0500

    Double quotes for f-string

--- train.py
+++ train.py
@@ -90,7 +90,7 @@ def get_or_build_tokenizer(config , ds , lang):
     return tokenizer
 
 def get_ds(config):
-    ds_raw = load_dataset("Helsinki-NLP/opus_books", f'{config['lang_src']} - {config['lang_tgt']}' , split = 'train')
+    ds_raw = load_dataset("Helsinki-NLP/opus_books", f"{config['lang_src']}-{config['lang_tgt']}", split='train')
     
     # build Tokenizer
     tokenizer_src = get_or_build_tokenizer(config , ds_raw , config['lang_src'])
