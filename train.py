1fefb0a0ddce4be28c9efc828788b5b7c9041cc3
Mubashir <cs.mubashir.a@gmail.com>
2025-03-10 06:04:02 +0500

    val_ds_size issue

--- train.py
+++ train.py
@@ -97,7 +97,7 @@ def get_ds(config):
     tokenizer_tgt = get_or_build_tokenizer(config , ds_raw , config['lang_tgt'])
 
     train_ds_size = int(0.9 * len(ds_raw))
-    val_ds_size = ds_raw - train_ds_size
+    val_ds_size = len(ds_raw) - train_ds_size
     train_ds_raw , val_ds_raw = random_split(ds_raw , [train_ds_size , val_ds_size])
     
     train_ds = BillingualDataset(train_ds_raw , tokenizer_src , tokenizer_tgt , config['lang_src'] , config['lang_tgt'] , config['seq_len'])
