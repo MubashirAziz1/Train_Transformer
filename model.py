0685971951f410519f9d4328101c68ba445472f9
Mubashir <cs.mubashir.a@gmail.com>
2025-03-11 06:01:16 +0500

    syntax error keep_dims

--- model.py
+++ model.py
@@ -46,8 +46,8 @@ class LayerNormalization(nn.Module):
         self.beta = nn.Parameter(torch.zeros(1)) # Added
     
     def forward(self , x):
-        mean = x.mean(dim = -1 , keep_dims = True)
-        std = x.std(dim = -1 , keep_dims = True)
+        mean = x.mean(dim = -1 , keepdim = True)
+        std = x.std(dim = -1 , keepdim = True)
         return self.alpha*(x-mean) / (std+self.eps)  + self.beta
 
 class FeedForwardBlock(nn.Module):
