a54f3b48f5151a4b1d240a0d3b2727e12cbd9c13
Mubashir <cs.mubashir.a@gmail.com>
2025-03-12 06:28:39 +0500

    projection

--- train.py
+++ train.py
@@ -446,7 +446,7 @@ def train_model(config):
             # Run the tensors through the encoder, decoder and the projection layer
             encoder_output = model.encode(encoder_input, encoder_mask) # (B, seq_len, d_model)
             decoder_output = model.decode(encoder_output, encoder_mask, decoder_input, decoder_mask) # (B, seq_len, d_model)
-            proj_output = model.project(decoder_output) # (B, seq_len, vocab_size)
+            proj_output = model.projection(decoder_output) # (B, seq_len, vocab_size)
 
             # Compare the output with the label
             label = batch['label'].to(device) # (B, seq_len)
